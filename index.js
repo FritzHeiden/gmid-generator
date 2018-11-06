992b624f658b499d62e8625bcd770bcd7ce9ba78
Fritz Heiden <fritz.heiden@fokus.fraunhofer.de>
2018-11-06 21:00:35 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const removeAccents = require('remove-accents')
+
+function generalizeString (string) {
+  return removeAccents(string)
+    .toLowerCase()
+    .replace(/\$/g, 's')
+    .replace(/€/g, 'e')
+    .replace(/¥/g, 'y')
+    .replace(/[^a-zA-Z0-9]+/g, '')
+}
+
+const GmidGenerator = {
+  generateArtistGmid (artistName) {
+    return generalizeString(artistName)
+  },
+  generateAlbumGmid (albumTitle, artistName) {
+    const artistGmid = GmidGenerator.generateArtistGmid(artistName)
+    const albumInfo = generalizeString(albumTitle)
+    return albumInfo + artistGmid
+  },
+  generateTrackGmid (trackTitle, trackNumber, albumTitle, artistName) {
+    if (!artistName) {
+      artistName = albumTitle
+      albumTitle = trackNumber
+      trackNumber = null
+    }
+    const albumGmid = GmidGenerator.generateAlbumGmid(albumTitle, artistName)
+    const trackInfo = generalizeString(trackTitle)
+
+    if (trackNumber) {
+      trackNumber = trackNumber + ''
+      trackNumber = trackNumber.padStart(2, '0')
+      return trackNumber + trackInfo + albumGmid
+    } else {
+      return '00' + trackInfo + albumGmid
+    }
+  }
+}
+
+module.exports = GmidGenerator
