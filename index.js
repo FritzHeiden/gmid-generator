35924f09287165099be42ddb02bbeaf0b1d63ab2
Fritz Heiden <fritz.heiden@fokus.fraunhofer.de>
2018-11-15 18:36:03 +0100

    handling invalid strings; added bare string generalizer method to api;

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const removeAccents = require('remove-accents')
 
-function generalizeString (string) {
-  return removeAccents(string)
-    .toLowerCase()
-    .replace(/\$/g, 's')
-    .replace(/€/g, 'e')
-    .replace(/¥/g, 'y')
-    .replace(/[^a-zA-Z0-9]+/g, '')
-}
-
 const GmidGenerator = {
+  generalizeString (string) {
+    if (!string) return string
+    return removeAccents(string)
+      .toLowerCase()
+      .replace(/\$/g, 's')
+      .replace(/€/g, 'e')
+      .replace(/¥/g, 'y')
+      .replace(/[^a-zA-Z0-9]+/g, '')
+  },
   generateArtistGmid (artistName) {
-    return generalizeString(artistName)
+    return GmidGenerator.generalizeString(artistName)
   },
   generateAlbumGmid (albumTitle, artistName) {
     const artistGmid = GmidGenerator.generateArtistGmid(artistName)
-    const albumInfo = generalizeString(albumTitle)
+    const albumInfo = GmidGenerator.generalizeString(albumTitle)
     return albumInfo + artistGmid
   },
   generateTrackGmid (trackTitle, trackNumber, albumTitle, artistName) {
@@ -25,7 +25,7 @@ const GmidGenerator = {
       trackNumber = null
     }
     const albumGmid = GmidGenerator.generateAlbumGmid(albumTitle, artistName)
-    const trackInfo = generalizeString(trackTitle)
+    const trackInfo = GmidGenerator.generalizeString(trackTitle)
 
     if (trackNumber) {
       trackNumber = trackNumber + ''
